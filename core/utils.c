2f9a7c65655a57939b9809bce0f6c06107a01d72
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-04-24 01:38:18 +0200

    .. navigation added

--- core/utils.c
+++ core/utils.c
@@ -1,7 +1,28 @@
 #include "fmheaders.h"
 
+int isDir(const char *path) {
+	DIR *dir;
+	dir = opendir(path);
+	if (dir == NULL) {
+		return 0;
+	}
+	closedir(dir);
+	return 1;
+}
 char *getFullPath(const char *path, const char *name) {
 	char *fpath;
-	asprintf(&fpath, "%s/%s", path, name);
+	if (!strcmp(name, "..")) {
+		int i = strlen(path) - 1;
+        asprintf(&fpath, "%s", path);
+
+		while (i--) {
+			if (fpath[i] == '/') {
+				fpath[i + 1] = 0;
+				break;
+			}
+		}
+	} else {
+        asprintf(&fpath, "%s%s/", path, name);
+    }
 	return fpath;
 }
