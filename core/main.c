27a553e384c4c688c5a635eba792a8f9989b1df3
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-04-24 01:39:25 +0200

    Minimal CLI for testing

--- core/main.c
+++ core/main.c
@@ -1,30 +1,52 @@
 #include "fmheaders.h"
+#include <string.h>
 
-char *getFullPath(const char *path, const char *name);
 int main(int argc, char *argv[]) {
 	struct passwd *pw = getpwuid(getuid());
 	const char *homedir = pw->pw_dir;
-	const char *path = homedir;
-	char choice;
+	char *path = (char *) malloc(1024);
+	path[0] = 0;
+	path = getFullPath(path, homedir);
+	char choice, str[BUFFSIZE], *fpath;
+	int show_hidden = 0;
 
 	while (1) {
-        listFiles(path, 1);
-        printf("\n> ");
+		puts("\n----------------------");
+		listFiles(path, show_hidden);
+		puts("\n----------------------");
+		printf("1. Make directory.\n2. Delete a file/directory.\n"
+			   "3. Change directory.\n4. Toggle hidden files/directories.\n");
+		printf("\n\\> ");
 		scanf("%c%*c", &choice);
-        printf("> ");
-		char str[BUFFSIZE], *fpath;
-        scanf("%[^\n]%*c", str);
-        fpath = getFullPath(path, str);
 		switch (choice) {
 			case '1':
-                makeDirectory(fpath);
+				printf("> ");
+				scanf("%[^\n]%*c", str);
+				fpath = getFullPath(path, str);
+				makeDirectory(fpath);
+				free(fpath);
 				break;
-            case '2':
-                deleteObject(fpath);
+			case '2':
+				printf("> ");
+				scanf("%[^\n]%*c", str);
+				fpath = getFullPath(path, str);
+				deleteObject(fpath);
+				free(fpath);
 				break;
-            default:
-                return 0;
+			case '3':
+				printf("> ");
+				scanf("%[^\n]%*c", str);
+				fpath = getFullPath(path, str);
+				if (isDir(fpath)) {
+					strcpy(path, fpath);
+				}
+				free(fpath);
+				break;
+			case '4':
+				show_hidden = !show_hidden;
+				break;
+			default:
+				return 0;
 		}
-        free(fpath);
 	}
 }
