456d2557a0de715e88d1eff679784fbee36d9b38
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-04-24 23:54:07 +0200

    CLI menu improved

--- cli/main.c
+++ cli/main.c
@@ -1,55 +1,48 @@
-#include "lotus.h"
 #include "lotus-cli.h"
+#include "lotus.h"
 
 int main(int argc, char *argv[]) {
 	char *current_path = getSettedPath();
-	char choice, *next_path;
+	char choice[5096], *next_path;
 	int show_hidden = 0;
 
 	while (1) {
-        displayMenu(current_path, show_hidden);
-		scanf("%c%*c", &choice);
-
-		switch (choice) {
-			case '1':
-                next_path = getNextPath(current_path);
-				makeDirectory(next_path);
-				free(next_path);
-				break;
-			case '2':
-                next_path = getNextPath(current_path);
-				deleteObject(next_path);
-				free(next_path);
-				break;
-			case '3':
-                next_path = getNextPath(current_path);
-				if (isDir(next_path)) {
-					strcpy(current_path, next_path);
-				} else {
-                    pcerror("\nError open directory");
-                }
-				free(next_path);
-				break;
-            case '4':
-                next_path = getNextPath(current_path);
-                char dst[4096];
-                printf("-> ");
-                scanf("%[^\n]%*c", dst);
-                createSymbolicLink(next_path, dst, current_path);
-                free (next_path);
-                break;
-            case '5':
-                next_path = getNextPath(current_path);
-                int mode;
-                scanf("%d%*c", &mode);
-                changePermissions(next_path, mode);
-				free(next_path);
-				break;
-			case '6':
-				show_hidden = !show_hidden;
-				break;
-			default:
-				return 0;
+		displayMenu(current_path, show_hidden);
+		scanf("%s%*[\040]", choice);
+		if (!strcmp(choice, "make")) {
+			next_path = getNextPath(current_path);
+			makeDirectory(next_path);
+			free(next_path);
+		} else if (!strcmp(choice, "remove")) {
+			next_path = getNextPath(current_path);
+			deleteObject(next_path);
+			free(next_path);
+		} else if (!strcmp(choice, "goto")) {
+			next_path = getNextPath(current_path);
+			if (isDir(next_path)) {
+				strcpy(current_path, next_path);
+			} else {
+				pcerror("\nError open directory");
+			}
+			free(next_path);
+		} else if (!strcmp(choice, "link")) {
+			next_path = getNextPath(current_path);
+			char *dst;
+			printf("-> ");
+            dst = getNextPath(0);
+			createSymbolicLink(next_path, dst, current_path);
+            free(dst);
+			free(next_path);
+		} else if (!strcmp(choice, "mode")) {
+			int mode;
+			scanf("%d%*[\040]", &mode);
+			next_path = getNextPath(current_path);
+			changePermissions(next_path, mode);
+			free(next_path);
+		} else if (!strcmp(choice, "hidden")) {
+			show_hidden = !show_hidden;
+		} else if (!strcmp(choice, "exit") || !strcmp(choice, "quit")) {
+            return 0;
 		}
 	}
 }
--- cli/menus.c
+++ cli/menus.c
@@ -9,16 +9,22 @@ void displayMenu(const char *path, int show_hidden) {
 	listFiles(path, show_hidden);
 	puts("\n----------------------");
 
-	printf("1. Make directory.\n2. Delete a file/directory.\n"
-		   "3. Change directory.\n4. Create symbolic link.\n"
-		   "5. Change permissions\n6. Toggle hidden files/directories.\n");
+	// printf("1. Make directory.\n2. Delete a file/directory.\n"
+	// 	   "3. Change directory.\n4. Create symbolic link.\n"
+	// 	   "5. Change permissions\n6. Toggle hidden files/directories.\n");
 
-	printf("\n\\> ");
+	printf("\\> ");
 }
 
 char *getNextPath(const char *path) {
 	char input[BUFFSIZE];
-	printf(">> ");
+	// printf(">> ");
 	scanf("%[^\n]%*c", input);
+    for (int i = strlen(input) - 1; i >= 0; i--) {
+        if (input[i] != ' ') {
+            input[i+1] = 0;
+            break;
+        }
+    }
 	return getFullPath(path, input);
 }
