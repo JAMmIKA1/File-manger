405209a5f3f782e1be4bee5da4bf99e8bf04a841
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-04-26 18:22:32 +0300

    COLOOORSSSS

--- core/list.c
+++ core/list.c
@@ -1,43 +1,55 @@
 #include "lotus.h"
+#include <stdio.h>
 
 void listFiles(const char *path, int show_hidden) {
 	DIR *dir;
 	struct dirent *entry;
-    unsigned dirs = 0, files = 0;
+	struct stat fileStat;
+	char fpath[4096];
+	unsigned dirs = 0, files = 0;
 
 	dir = opendir(path);
 	if (dir == NULL) {
-        pcerror("\nError opening directory");
+		pcerror("\nError opening directory");
 		return;
 	}
 
-    // Print directories first
-    printf("\033[1;35m");
+	// Print directories first
 	while ((entry = readdir(dir)) != NULL) {
 		if (entry->d_type == 4 && strcmp(entry->d_name, "..") &&
 			strcmp(entry->d_name, ".") &&
 			(show_hidden || entry->d_name[0] != '.')) {
-			printf("%s/\n", entry->d_name);
-            dirs++;
+			printf("\033[1;35m%s\033[0m/\n", entry->d_name);
+			dirs++;
 		}
 	}
 
-    // Print files
-    printf("\033[0m");
-    rewinddir(dir);
+	// Print files
+	printf("\033[0m");
+	rewinddir(dir);
+
 	while ((entry = readdir(dir)) != NULL) {
+		char specialchar = 0;
 		if (entry->d_type != 4 && strcmp(entry->d_name, "..") &&
 			strcmp(entry->d_name, ".") &&
 			(show_hidden || entry->d_name[0] != '.')) {
-            if (entry->d_type == 10) {
-                printf("\033[1;36m");
-            }
-			printf("%s%c\033[0m\n", entry->d_name,
-				   ((entry->d_type == 10) ? '@' : 0));
-            files++;
+			snprintf(fpath, 4096, "%s/%s", path, entry->d_name);
+			stat(fpath, &fileStat);
+			if (entry->d_type == 10) {
+				printf("\033[1;36m");
+				specialchar = '@';
+			} else {
+				if (fileStat.st_mode & S_IXUSR || fileStat.st_mode & S_IXGRP ||
+					fileStat.st_mode & S_IXOTH) {
+					printf("\033[1;32m");
+					specialchar = '*';
+				}
+			}
+			printf("%s\033[0m%c\n", entry->d_name, specialchar);
+			files++;
 		}
 	}
 
-    printf("\n%u directories, %u files", dirs, files);
+	printf("\n%u directories, %u files", dirs, files);
 	closedir(dir);
 }
