83030f6ca751a374cc257b836070e626d965a6c6
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-04-27 14:15:15 +0300

    Symbolic link deletion problems

--- core/manage.c
+++ core/manage.c
@@ -1,28 +1,30 @@
 #include "lotus.h"
+char tmpbuff[4096];
 
 void makeDirectory(const char *path) {
-    if (mkdir(path, 0777) == -1) {
-        pcerror("\nError creating directory");
-    }
+	if (mkdir(path, 0777) == -1) {
+		pcerror("\nError creating directory");
+	}
 }
 
 void deleteObject(const char *path) {
-    DIR *dir = opendir(path);
-    struct dirent *entry;
+	DIR *dir = opendir(path);
+	struct dirent *entry;
+	ssize_t link_len = readlink(path, tmpbuff, sizeof(tmpbuff) - 1);
 
-    // recursively delete all entities inside the directory
-    if (dir) {
-        while ((entry = readdir(dir)) != NULL) {
-            if (strcmp(entry->d_name, "..") && strcmp(entry->d_name, ".")) {
-                char *fpath = getFullPath(path, entry->d_name);
-                deleteObject(fpath);
-                free(fpath);
-            }
-        }
-        closedir(dir);
-    }
+	// recursively delete all entities inside the directory
+	if (dir && link_len == -1) {
+		while ((entry = readdir(dir)) != NULL) {
+			if (strcmp(entry->d_name, "..") && strcmp(entry->d_name, ".")) {
+				char *fpath = getFullPath(path, entry->d_name);
+				deleteObject(fpath);
+				free(fpath);
+			}
+		}
+		closedir(dir);
+	}
 
-    if (remove(path) == -1) {
-        pcerror("\nError deleting file");
-    }
+	if (remove(path) == -1) {
+		pcerror("\nError deleting file");
+	}
 }
