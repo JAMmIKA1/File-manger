cfdd45fdb8b5e1b0a927242bb5f6db8bfe4f2ddc
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-05-10 17:35:50 +0300

    missing signatures

--- core/lotus.h
+++ core/lotus.h
@@ -1,27 +1,31 @@
 #ifndef LOTUS_H
 #define LOTUS_H
+#define BUFFSIZE 4096
+#define ST 2
 
 #include <dirent.h>
+#include <unistd.h>
 #include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <unistd.h>
-#define BUFFSIZE 4096
-#define ST 2
 
-void trim(char *input);
-void parsePath(char *str);
 void listFiles(const char *path, int show_hidden);
-char *getFullPath(const char *path, char *name);
-int isDir(const char *path);
 void changePermissions(const char *path, unsigned int mode);
 void makeDirectory(const char *path);
 void deleteObject(const char *path);
 void createSymbolicLink(char *src, const char *current_path);
+
+char *getFullPath(const char *path, char *name);
+char *getNextPath(char *path);
+char *getSettedPath();
 char *getHome();
+
+void trim(char *input);
 void pcerror(const char *err);
+void parsePath(char *str);
+int isDir(const char *path);
 
 #endif
